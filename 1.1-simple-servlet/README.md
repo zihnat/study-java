dd87b4c130840fc4721020d74e071c9e25faf832
zihnat <iz@z-book.local>
2018-03-28 18:00:04 +0300

    example 1: simple servlet

---
+++ 1.1-simple-servlet/README.md
@@ -0,0 +1,38 @@
+# SimpleServlet project
+
+Source code for servlet class:
+```
+src/net/java/servlet/SimpleServlet.java
+```
+
+Source code for .jsp file:
+```
+WebContent/index.jsp
+```
+
+File web.xml:
+```
+WebContent/WEB-INF/web.xml
+```
+
+All following commands are running from project folder.
+
+## Compile project
+
+Create compiled java class file in folder ```WebContent/WEB-INF/classes/``` by next command:
+```
+javac -cp TOMCAT_FOLDER/lib/servlet-api.jar -d ./WebContent/WEB-INF/classes/ ./src/net/java/servlet/SimpleServlet.java
+```
+
+## Make war file
+
+Create .war file in folder ```deploy``` by next command:
+```
+jar cfv deploy/SimpleServletApp.war -C WebContent .
+```
+
+## Deploy the web application
+Deploy war file to tomcat's ```webapps``` folder:
+```
+cp deploy/SimpleServletApp.war TOMCAT_FOLDER/webapps/
+```
---
+++ 1.1-simple-servlet/src/net/java/servlet/SimpleServlet.java
@@ -0,0 +1,55 @@
+package net.java.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SimpleServlet extends HttpServlet {
+
+    /**
+     * this life-cycle method is invoked when this servlet is first accessed
+     * by the client
+     */
+    public void init(ServletConfig config) {
+        System.out.println("Servlet is being initialized");
+    }
+
+    /**
+     * handles HTTP GET request
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>Hello from Simple Java servlet!</html>");
+        writer.flush();
+    }
+
+    /**
+     * handles HTTP POST request
+     */
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        String paramName = request.getParameter("name");
+
+        String paramAge = request.getParameter("age");
+        int age = Integer.parseInt(paramAge);
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>Hello " + paramName + " in age of " + age + "</html>");
+        writer.flush();
+
+    }
+ 
+    /**
+     * this life-cycle method is invoked when the application or the server
+     * is shutting down
+     */
+    public void destroy() {
+        System.out.println("Servlet is being destroyed");
+    }
+}
