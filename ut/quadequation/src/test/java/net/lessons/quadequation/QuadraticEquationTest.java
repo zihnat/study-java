1069bcf470769c6e600becafabfd8acc5e54a670
zihnat <iz@z-book.local>
2018-06-04 10:19:13 +0300

    junit for quadratic equation v.7

--- ut/quadequation/src/test/java/net/lessons/quadequation/QuadraticEquationTest.java
+++ ut/quadequation/src/test/java/net/lessons/quadequation/QuadraticEquationTest.java
@@ -9,43 +9,47 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class QuadraticEquationTest{
-  private static IQuadraticEquation qe;
 
-  @BeforeClass
-  public static void initQuadraticEquation() {
-    qe = new QuadraticEquation();
+  @Test
+  public void test_init_QuadraticEquation() {
+    IQuadraticEquation qe = new QuadraticEquation();
   }
 
   @Test
-  public void test_solve() throws Exception{
+  public void test_solve_attr_m1_1_0() throws Exception{
+    IQuadraticEquation qe = new QuadraticEquation();
     double[] expected = {-1.0, 0.0};
     double[] res = qe.solve(1, 1, 0);
     assertArrayEquals(expected, res, 0.01);
   }
 
   @Test
-  public void test_solve() throws Exception{
+  public void test_solve_attr_1_m1_0() throws Exception{
+    IQuadraticEquation qe = new QuadraticEquation();
     double[] expected = {0.0, 1.0};
     double[] res = qe.solve(1, -1, 0);
     assertArrayEquals(expected, res, 0.01);
   }
 
   @Test
-  public void test_solve() throws Exception{
+  public void test_solve_attr_1_0_m1() throws Exception{
+    IQuadraticEquation qe = new QuadraticEquation();
     double[] expected = {-1.0, 1.0};
     double[] res = qe.solve(1.0, 0, -1.0);
     assertArrayEquals(expected, res, 0.01);
   }
 
   @Test
-  public void test_solve() throws Exception{
+  public void test_solve_attr_1_0_m4() throws Exception{
+    IQuadraticEquation qe = new QuadraticEquation();
     double[] expected = {-2.0, 2.0};
     double[] res = qe.solve(1.0, 0, -4.0);
     assertArrayEquals(expected, res, 0.01);
   }
 
   @Test
-  public void test_solve() throws Exception{
+  public void test_solve_attr_3_6_m24() throws Exception{
+    IQuadraticEquation qe = new QuadraticEquation();
     double[] expected = {-4.0, 2.0};
     double[] res = qe.solve(3, 6 , -24);
     assertArrayEquals(expected, res, 0.01);
@@ -53,11 +57,13 @@ public class QuadraticEquationTest{
 
   @Test(expected = Exception.class)
   public void test_solve_exception_not_quadratic() throws Exception{
+    IQuadraticEquation qe = new QuadraticEquation();
     double[] res = qe.solve(0.0, 6.0, -24.0);
   }
 
   @Test(expected = Exception.class)
   public void test_solve_exception_complex_result() throws Exception{
-      double[] res = qe.solve(1.0, 1.0, 1.0);
+    IQuadraticEquation qe = new QuadraticEquation();
+    double[] res = qe.solve(1.0, 1.0, 1.0);
   }
 }
